a3318379ffb71950756660c71e51b53a2723c324
akshaykumar333 <64579795+akshaykumar333@users.noreply.github.com>
2020-05-01 13:11:14 +0530

    Update Test2.java

--- Test/src/org/java/TestApp/Test2.java
+++ Test/src/org/java/TestApp/Test2.java
@@ -4,7 +4,10 @@ public class Test2 {
 	
 	public class Hello
 	{
-		
+		public static second()
+		{
+			System.out.println("nested class method");
+		}
 	}
 	
 	    public static void main(String[] args) 
