af8a8060df7829507d34070c82dbadda932d0a72
Akshay <Akshay@LATTITUDEE6400>
2020-05-01 13:08:17 +0530

    added Test2 class

---
+++ Test/src/org/java/TestApp/Test2.java
@@ -0,0 +1,17 @@
+package org.java.TestApp;
+
+public class Test2 {
+	
+	public class Hello
+	{
+		
+	}
+	
+	    public static void main(String[] args) 
+	       {
+
+		    	System.out.println("hello java");
+		        
+		    }
+
+}
